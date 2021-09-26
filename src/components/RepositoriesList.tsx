4b7341565605611755ba6ad825561bdde5250279
Armando Castillo <31118679+MandoCastillo@users.noreply.github.com>
2021-09-26 13:39:06 -0500

    useTypedSelector

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,24 +1,30 @@
 import React, { FC, useState } from 'react';
 import { useActions } from '../hooks/useActions';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 
 interface RepositoriesListProps {
 }
 
 export const RepositoriesList: FC<RepositoriesListProps> = () => {
-  const [term, setTerm] = useState<string>('')
-   const { searchRepositories } = useActions()
+  const [term, setTerm] = useState<string>('');
+  const {searchRepositories} = useActions();
+  const {data, loading, error} = useTypedSelector(state => state.repositories);
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    searchRepositories(term)
-  }
+    searchRepositories(term);
+  };
 
   return (
-    <form onSubmit={onSubmit}>
-      <input type="text" value={term} onChange={e=> setTerm(e.target.value)} />
-      <button>Search</button>
-    </form>
-
+    <>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={term} onChange={e => setTerm(e.target.value)} />
+        <button>Search</button>
+      </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map(name => <div key={name}>{name}</div>)}
+    </>
   );
 };
